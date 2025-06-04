fe8c31602d95167d7e65633ebb3fd2723d6bfb7b
Josh Bensimon <jjbensimon@me.com>
2025-06-04 20:01:04 +0200

    fix: branding v2

--- apps/remix/app/utils/meta.ts
+++ apps/remix/app/utils/meta.ts
@@ -2,11 +2,11 @@ import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 
 export const appMetaTags = (title?: string) => {
   const description =
-    'Join Diploma Santé, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
+    'Join Prepa Prevision, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
 
   return [
     {
-      title: title ? `${title} - Diploma Santé` : 'Diploma Santé',
+      title: title ? `${title} - Prepa Prevision` : 'Prepa Prevision',
     },
     {
       name: 'description',
@@ -14,12 +14,11 @@ export const appMetaTags = (title?: string) => {
     },
     {
       name: 'keywords',
-      content:
-        'Signature Diploma Santé',
+      content: 'Signature Prepa Prevision',
     },
     {
       name: 'author',
-      content: 'Diploma Santé',
+      content: 'Prepa Prevision',
     },
     {
       name: 'robots',
@@ -27,7 +26,7 @@ export const appMetaTags = (title?: string) => {
     },
     {
       property: 'og:title',
-      content: 'Diploma Santé - Signature',
+      content: 'Prepa Prevision - Signature',
     },
     {
       property: 'og:description',
